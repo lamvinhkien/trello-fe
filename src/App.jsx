cd4ab7f64ff9488672309f6d51f7b6c7c1ecf7c7
lamvinhkien <lamvinhkien1709@gmail.com>
2025-02-22 21:46:52 +0700

    dark light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import Tabs from '@mui/material/Tabs'
+import { useColorScheme } from '@mui/material/styles'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
 
 function App() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
     <>
-      <div>lamvinhkien</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <Tabs />
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="dark-light-mode-label">Mode</InputLabel>
+        <Select
+          labelId="dark-light-mode-label"
+          id="dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value='light'>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '8px' }}>
+              <LightModeIcon fontSize='small' />
+              <div>
+                Light
+              </div>
+            </div>
+          </MenuItem>
+          <MenuItem value='dark'>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '8px' }}>
+              <DarkModeOutlinedIcon fontSize='small' />
+              <div>
+                Dark
+              </div>
+            </div>
+          </MenuItem>
+          <MenuItem value='system'>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '8px' }}>
+              <SettingsBrightnessOutlinedIcon fontSize='small' />
+              <div>
+                System
+              </div>
+            </div>
+          </MenuItem>
+        </Select>
+      </FormControl>
     </>
   )
 }
