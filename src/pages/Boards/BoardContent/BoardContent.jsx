93ff981341ef06a210e8f49eb5a1f001d57b6023
lamvinhkien <lamvinhkien1709@gmail.com>
2025-07-08 21:29:23 +0700

    video 30

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,19 +1,45 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 const BoardContent = ({ board }) => {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+    if (!over) return
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
 
   return (
-    <Box sx={{
-      width: '100%',
-      height: theme => theme.trello.boardContentHeight,
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      p: '8px 0'
-    }}>
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        width: '100%',
+        height: theme => theme.trello.boardContentHeight,
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        p: '8px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
